b273e6afad4ca5504fa57c08b108bf277ac38251
Timple Basinger <tbasinger@protonmail.com>
2021-09-22 17:11:04 -0500

    Initial link to GH

---
+++ app.js
@@ -0,0 +1,19 @@
+// variable definition of my name
+let myName = "Timple";
+console.log('My name is '  + myName + '.');  
+
+// Const naming # of states in the US
+const numberOfStates = "50";
+console.log('The number of US states is ' + numberOfStates + '.');
+
+//Result of adding 5 + 4 two ways 
+let w = 5;
+w += 4;
+console.log(w); 
+
+let result = 5 + 4;
+console.log(result);
+
+function sayHello() {
+    alert("Hello World!");
+}
